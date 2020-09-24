5ca1715c81506c4b69b5c9741b5efd431ea922fa
IamHerica <hericacadoni@gmail.com>
2020-09-23 23:00:30 -0300

    Criando main.js e utilizando console.log
    Neste commit, vi o basico de variaveis, alert e utilizei o console.log

---
+++ main.js
@@ -0,0 +1,14 @@
+var nome = "Herica";
+var idade = 5;
+var idade2= 3;
+var frase = "Sera que estou aprendendo?"
+alert(nome + " tem " +idade + " anos");
+//alert(idade * idade2);
+console.log(nome);
+console.log(idade);
+console.log(frase.replace("Sera que estou aprendendo", "Voce esta aprendendo!"));
+console.log(frase.toUpperCase());
+console.log(idade * idade2);
+//toUpperCase = tudo p maisculo
+//oLocaleLowerCase = tudo p minusculo
+ 
