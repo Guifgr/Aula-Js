220bbd6f1b4d4edf758182bc4b727dbccf920575
IamHerica <hericacadoni@gmail.com>
2020-10-02 14:15:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-var nome = "Herica";
+var lista = ["maca", "pera"];
+lista.push("uva");
+console.log(lista[1])
+
+
+/*var nome = "Herica";
 var idade = 5;
 var idade2= 3;
 var frase = "Sera que estou aprendendo?"
@@ -9,6 +14,6 @@ console.log(idade);
 console.log(frase.replace("Sera que estou aprendendo", "Voce esta aprendendo!"));
 console.log(frase.toUpperCase());
 console.log(idade * idade2);
+/*
 //toUpperCase = tudo p maisculo
 //oLocaleLowerCase = tudo p minusculo
- 
