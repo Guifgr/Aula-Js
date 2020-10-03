e62ecc64ead4bfc414718a7e988a765ef57a86e2
guifgr <guifgr@hotmail.com>
2020-10-02 22:34:19 -0300

    add visual

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+function getText(){
+    const nome = document.getElementById("nome").value
+    const idade = document.getElementById("idade").value
+    document.getElementById("h1").innerHTML = " Meu nome é "+nome+ ", tenho "+idade+" anos!";
+}
+
 var lista = ["maca", "pera"];
 lista.push("uva");
 console.log(lista[1])
 
 
-/*var nome = "Herica";
+var nome = "Herica";
 var idade = 5;
 var idade2= 3;
 var frase = "Sera que estou aprendendo?"
@@ -14,6 +20,3 @@ console.log(idade);
 console.log(frase.replace("Sera que estou aprendendo", "Voce esta aprendendo!"));
 console.log(frase.toUpperCase());
 console.log(idade * idade2);
-/*
-//toUpperCase = tudo p maisculo
-//oLocaleLowerCase = tudo p minusculo
